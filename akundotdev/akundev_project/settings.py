b9b9ef47d532901e9d75ce92a44da7649f566aea
Almaz Kunpeissov <almaz.kun@gmail.com>
2020-03-03 18:19:07 +0900

    moving django in inner file

--- akundotdev/akundev_project/settings.py
+++ akundotdev/akundev_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "bv9jy85kq0nyh$n5+_7v@s8wgv+w3-)kx-ij!=gs%e7&(y666e"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
