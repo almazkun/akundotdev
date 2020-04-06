4ec15e7d25a980f688a91e5f99755ed21aea1b37
Almaz Kunpeissov <almaz.kun@gmail.com>
2020-04-06 09:16:28 +0900

    front end style changed, links to static moved to pages

--- akundotdev/akundev_project/settings.py
+++ akundotdev/akundev_project/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", default="asdasdcnjx289rm34yc3487cny5cb8")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="*")
 
 
 # Application definition
