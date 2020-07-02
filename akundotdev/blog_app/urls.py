7bc86a9355c14fb117f7d3f571e72bf61926dcb1
Almaz Kunpeissov <almaz.kun@gmail.com>
2020-07-02 12:55:37 +0900

    reqrite of the site. new django project

--- akundotdev/blog_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import ListView, DetailView
-
-
-urlpatterns = [
-    path("articles/", ListView.as_view(), name="articles"),
-    path("articles/<slug:slug>/", DetailView.as_view(), name="article_detail"),
-]
