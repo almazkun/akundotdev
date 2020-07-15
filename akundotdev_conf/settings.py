2a25799f895da414aa7aaf18e46c1225ed41dd50
Almaz Kunpeissov <almaz.kun@gmail.com>
2020-07-15 18:21:24 +0900

    os environ setting added

--- akundotdev_conf/settings.py
+++ akundotdev_conf/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+try: 
+    import secret
+    DB_PASSWORD = secret.DB_PASSWORD
+except:
+    DB_PASSWORD = ""
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,7 +97,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "django_db",
         "USER": "djangouser",
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "PASSWORD": DB_PASSWORD,
         "HOST": "",
         "PORT": "",
     }
